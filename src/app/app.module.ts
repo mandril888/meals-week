a63b10f48d1423fa1748affdf74d262226ac8990
Arturo Rubio <arubio@seocom.agency>
2019-04-05 19:14:38 +0200

    copy tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-
-import { FormsModule } from '@angular/forms';
-
-
-import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { MealsComponent } from './meals/meals.component';
-import { MealComponent } from './meals/meal/meal.component';
-import { MealListComponent } from './meals/meal-list/meal-list.component';
-import { MealService } from './shared/meal.service';
+import { environment } from 'src/environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeService } from './shared/employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MealsComponent,
-    MealComponent,
-    MealListComponent
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    NgbModule.forRoot(),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [MealService],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/meal.ts
+++
@@ -1,6 +0,0 @@
-export class Meal {
-    id: string;
-    name: string; // nombre de la comida
-    type: string; // primero, segundo, poste
-    sano: string;
-}
